edc3e9d1fbd490700c5748f8bbf4c08708622e6f
Igor Dias <igordiasth@gmail.com>
2020-04-09 14:31:11 +1200

    config webpack, run mode development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
-        
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
       }
     ]
   }
