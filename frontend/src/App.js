12117f547016d2c1edb0abba5a49820231ef642f
Igor Dias <igordiasth@gmail.com>
2020-04-09 15:39:01 +1200

    react props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,19 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Header />
-    </div>
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Hompage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
   );
 }
 
