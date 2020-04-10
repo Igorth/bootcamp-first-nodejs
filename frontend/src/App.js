1d3a7a0ca7e55f2427597d28dda0139aac3f9cfd
Igor Dias <igordiasth@gmail.com>
2020-04-10 14:19:35 +1200

    app css, file loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
+import './App.css';
+
 import Header from './components/Header';
+
 
 function App() {
   const [projects, setProjects] = useState(['Developer App', 'Init front-end web']);
